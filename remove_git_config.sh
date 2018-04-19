24876a0b845711bde1cc09bafe36b2b42d66aade
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-19 15:58:41 -0300

    Usar sed pra remover linhas

--- remove_git_config.sh
+++ remove_git_config.sh
@@ -1,4 +1,9 @@
 #! bin/bash
 
 rm -f ~/.gitconfig && touch ~/.gitconfig
+
 LOCAL_CONFIG=$(find /home/ -regex '.*[/]nomeDoProjeto[/].git[/]config' -print -quit)
+
+sed -i '/\[user\]/d' $LOCAL_CONFIG
+sed -i '[ \t]name.*' $LOCAL_CONFIG
+sed -i '[ \t]email.*' $LOCAL_CONFIG
