4d19de470c8bf6ce9519eb1bd5f1a427772bf8a4
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-19 15:59:22 -0300

    Usar como regex

--- remove_git_config.sh
+++ remove_git_config.sh
@@ -5,5 +5,5 @@ rm -f ~/.gitconfig && touch ~/.gitconfig
 LOCAL_CONFIG=$(find /home/ -regex '.*[/]nomeDoProjeto[/].git[/]config' -print -quit)
 
 sed -i '/\[user\]/d' $LOCAL_CONFIG
-sed -i '[ \t]name.*' $LOCAL_CONFIG
-sed -i '[ \t]email.*' $LOCAL_CONFIG
+sed -i '/[ \t]name.*/d' $LOCAL_CONFIG
+sed -i '/[ \t]email.*/d' $LOCAL_CONFIG
