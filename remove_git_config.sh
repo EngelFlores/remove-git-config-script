c700a0d880e43a518bd8773d6f069259e9e2d1ef
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-19 21:55:51 -0300

    Melhorando o regex

--- remove_git_config.sh
+++ remove_git_config.sh
@@ -1,9 +1,5 @@
 #! bin/bash
 
 truncate -s 0 ~/.gitconfig
-
-LOCAL_CONFIG=$(find /home/ -regex '.*[/]nomeDoProjeto[/].git[/]config' -print -quit)
-
-sed -i '/\[user\]/d' $LOCAL_CONFIG
-sed -i '/[ \t]name.*/d' $LOCAL_CONFIG
-sed -i '/[ \t]email.*/d' $LOCAL_CONFIG
+LOCAL_CONFIG=$(find /Users/ghidorsi -regex '.*[/]nomeDoProjeto[/].git[/]config' -print -quit)
+sed -Ei '/(user|name.*|email.*)/d' $LOCAL_CONFIG
