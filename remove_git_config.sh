ccec768d815c497a26d26d2ea0b1419896a39dbc
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-05-03 10:33:25 -0300

    Usando o nome do projeto, só precisa fazer crontab agora

--- remove_git_config.sh
+++ remove_git_config.sh
@@ -1,5 +1,6 @@
 #! bin/bash
 
 truncate -s 0 ~/.gitconfig
-LOCAL_CONFIG=$(find /Users/ghidorsi -regex '.*[/]nomeDoProjeto[/].git[/]config' -print -quit)
+
+LOCAL_CONFIG=$(find /Users/ghidorsi -regex '.*[/]prescare[/]config' -print -quit)
 sed -Ei '/(user|name.*|email.*)/d' $LOCAL_CONFIG
