61e44019d9424ed90a13da3ec96d7e1f3dadc8d0
ghidorsi <ghidorsi@thoughtworks.com>
2018-05-09 12:32:00 -0300

    Fix

--- remove_git_config.sh
+++ remove_git_config.sh
@@ -2,5 +2,5 @@
 
 truncate -s 0 ~/.gitconfig
 
-LOCAL_CONFIG=$(find /Users/ghidorsi -regex '.*[/]prescare[/]config' -print -quit)
+LOCAL_CONFIG=$(find ~/ -regex '.*[/]prescare[/][.]git[/]config' -print -quit)
 sed -Ei '/(user|name.*|email.*)/d' $LOCAL_CONFIG
